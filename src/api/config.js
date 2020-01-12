76390202f79e7902c9764f6bd1b672b0bcb3d2e6
panshicheng <44695728+Shi-Cheng@users.noreply.github.com>
2020-01-12 18:41:07 +0800

    Add files via upload

---
+++ src/api/config.js
@@ -0,0 +1,37 @@
+export const commonParamsRecommend = {
+  cgiKey: 'GetHomePage',
+  data: {
+    comm: {
+      g_tk: 5381,
+      uin: '',
+      format: 'json',
+      inCharset: 'utf-8',
+      outCharset: 'utf-8',
+      notice: 0,
+      platform: 'h5',
+      needNewCode: 1 },
+    MusicHallHomePage: {
+      module: 'music.musicHall.MusicHallPlatform',
+      method: 'MobileWebHome',
+      param: { 'ShelfId': [101, 102, 161] }},
+    hotkey: {
+      module: 'tencent_musicsoso_hotkey.HotkeyService',
+      method: 'GetHotkeyForQQMusicMobile',
+      param: {
+        remoteplace: 'txt.miniapp.wxada7aab80ba27074',
+        searchid: '1559616839293' }
+    }}
+}
+export const commonParams = {
+  g_tk: 1928093487,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
