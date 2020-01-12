18cd20becd38a4ae75763f72cd2a5930525b442d
penov <x_pan2016@163.com>
2020-01-12 20:10:28 +0800

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,13 @@
       <router-view></router-view>
     </keep-alive>
   </div>
+
 </template>
 
 <script type="text/ecmascript-6">
   import MHeader from 'components/m-header/m-header'
   import Tab from 'components/tab/tab'
-//
+  //
   export default {
     components: {
       MHeader,
